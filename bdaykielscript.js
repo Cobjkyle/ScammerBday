bd0fc41d037a83c73d682eb47734e5d410f4d94d
Cobjkyle <jkbaldon28@gmail.com>
2026-05-22 11:57:36 +0800

    Add files via upload

---
+++ bdaykielscript.js
@@ -0,0 +1,13 @@
+
+function fakescam(){
+  alert("Congratulations! You have won a free iPhone 17 Pro Max! Click OK to claim your prize!");
+  alert("EHeyyy joke lang, umasa ka baaa");
+  alert("Dahil clinick mo mahahack naa kita");
+  alert("Kuha ko na password pati ip add mo");
+  alert("Pero kung ayaw mo, click mo ule ok para macancel hack. Real no scam");
+    let screen = document.getElementById("blackbg");
+
+  screen.style.opacity = "0";
+  screen.style.pointerEvents = "none";
+
+}
